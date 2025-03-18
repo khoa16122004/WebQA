3ac88035f2ae518afc9a079bb781b4366ed2ac94
khoa16122004 <khoatr.work@gmail.com>
2025-03-18 13:36:28 +0700

    update

--- proccess.py
+++ proccess.py
@@ -6,12 +6,22 @@ from tqdm import tqdm
 
 tsv_path = "extracted_images/imgs.tsv"
 output_dir = "images"
+fail_path = "fail.txt"
+fail_list = []
 os.makedirs(output_dir, exist_ok=True)
 with open(tsv_path, "r") as tsv_file:
     data = [line.strip().split("\t") for line in tsv_file.readlines()]
 
 for i in tqdm(range(len(data))):
     imgid, img_base64 = data[i]
-    im = Image.open(BytesIO(base64.b64decode(img_base64))).convert("RGB")
-    im.save(os.path.join(output_dir, f"{imgid}.png"))
-    
+    try:
+        img_path = os.path.join(output_dir, f"{imgid}.png")
+        if os.path.exists(img_path):
+            im = Image.open(BytesIO(base64.b64decode(img_base64))).convert("RGB")
+            im.save(img_path)
+    except:
+        fail_list.append(imgid)
+
+with open(fail_path, "w") as f:
+    f.write("\n".join(fail_list))
+        
