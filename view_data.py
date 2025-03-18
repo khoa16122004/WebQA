47cb9e81c4f71cc44637b0414597e37699186ae7
khoa16122004 <khoatr.work@gmail.com>
2025-03-18 13:48:26 +0700

    Update view_data.py

---
+++ view_data.py
@@ -0,0 +1,44 @@
+import os
+import json
+import random
+
+def view_data(sample):
+    img_posFacts = sample['img_posFacts']
+    img_negFacts = sample['img_negFacts']
+    txt_posFacts = sample['txt_posFacts']
+    txt_negFacts = sample['txt_negFacts']
+    
+    print("---------------Image Postive Document--------------------")
+    for item in img_posFacts:
+        print(f"{item['image_id']} - {item['caption']}")
+    
+    print("---------------Image Negative Document--------------------")
+    for item in img_negFacts:
+        print(f"{item['image_id']} - {item['caption']}")
+        
+    print("---------------Txt Postive Document--------------------")
+    for item in txt_posFacts:
+        print(f"{item['snippet_id']} - {item['fact']}")
+    
+    print("---------------Txt Negative Document--------------------")
+    for item in txt_negFacts:
+        print(f"{item['snippet_id']} - {item['fact']}")
+        
+def main():
+    path = "extracted_images/WebQA_data/WebQA_train_val.json"
+    with open(path, "r") as f:
+        data = json.load(f)
+        keys = data.keys()
+
+    while True:
+        print("----------------------------------- Done -----------------------------")
+        selection = int(input("Choose: "))
+        if selection == -1:
+            print("Done")
+        
+        else:
+            sample_id = random.randint(0, len(data))
+            sample_key = keys[sample_id]
+            view_data(data[sample_key])
+    
+    
