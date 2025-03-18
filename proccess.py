6e9d97c4bac331bad311efec4742e881e6801476
khoa16122004 <khoatr.work@gmail.com>
2025-03-18 12:12:32 +0700

    Update proccess.py

--- proccess.py
+++ proccess.py
@@ -7,10 +7,9 @@ from tqdm import tqdm
 tsv_path = "extracted_images/imgs.tsv"
 output_dir = "images"
 os.makedirs(output_dir, exist_ok=True)
-
 with open(tsv_path, "r") as tsv_file:
-    imgids = [line.strip().split("\t")[0] for line in tsv_file]
-    img_base64s = [line.strip().split("\t")[1] for line in tsv_file]
+    imgids = [line.strip().split("\t")[0] for line in tsv_file.readlines()]
+    img_base64s = [line.strip().split("\t")[1] for line in tsv_file.readlines()]
 
 for i in tqdm(range(len(imgids))):
     imgid = imgids[i]
