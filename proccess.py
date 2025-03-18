d2ec01c07a3b02d7bd9623313f49f46e4a8be68f
khoa16122004 <khoatr.work@gmail.com>
2025-03-18 12:06:10 +0700

    Create proccess.py

---
+++ proccess.py
@@ -0,0 +1,19 @@
+import os
+import base64
+from PIL import Image
+from io import BytesIO
+from tqdm import tqdm
+
+tsv_path = "imgs.tsv"
+output_dir = "images"
+
+with open(tsv_path, "r") as tsv_file:
+    imgids = [line.strip().split("\t")[0] for line in tsv_file]
+    img_base64s = [line.strip().split("\t")[1] for line in tsv_file]
+
+for i in tqdm(range(len(imgids))):
+    imgid = imgids[i]
+    img_base64 = img_base64s[i]
+    im = Image.open(BytesIO(base64.b64decode(img_base64)))
+    im.save(os.path.join(output_dir, f"{imgid}.jpg"))
+    
