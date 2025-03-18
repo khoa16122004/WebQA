4c6b183ae8fc6cd3b69e026e7b7435bd05922761
khoa16122004 <khoatr.work@gmail.com>
2025-03-18 12:27:59 +0700

    Update proccess.py

--- proccess.py
+++ proccess.py
@@ -12,6 +12,6 @@ with open(tsv_path, "r") as tsv_file:
 
 for i in tqdm(range(len(data))):
     imgid, img_base64 = data[i]
-    im = Image.open(BytesIO(base64.b64decode(img_base64)))
-    im.save(os.path.join(output_dir, f"{imgid}.jpg"))
+    im = Image.open(BytesIO(base64.b64decode(img_base64))).convert("RGB")
+    im.save(os.path.join(output_dir, f"{imgid}.png"))
     
