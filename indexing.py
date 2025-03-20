e7cc6bc63d60b44825cbba611ec92f19dfecb9a1
khoa16122004 <khoatr.work@gmail.com>
2025-03-20 15:07:37 +0700

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -30,7 +30,7 @@ class VectorDB:
 
             self.image_index = faiss.read_index(image_index_path)
             self.image_index = faiss.index_cpu_to_gpu(res,
-                                                      "cuda",
+                                                      "0",
                                                       self.image_index)
             print("Image vectors db len: ", self.image_index.ntotal)
         if txt_index_path:
