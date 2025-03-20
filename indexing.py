b454c97e74443932b345c77ce63402fc5c3c949d
khoa16122004 <khoatr.work@gmail.com>
2025-03-20 14:17:26 +0700

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -76,9 +76,7 @@ class VectorDB:
             # img.save("test.png")
             imgs.append(self.preprocess(img))
             break
-        print(imgs[0].shape)
         imgs = torch.stack(imgs).cuda()
-        print(imgs.shape)
         
         print("Extract features of image:  ")
 
@@ -93,7 +91,10 @@ class VectorDB:
                     
                 imgs_features = self.model.encode_image(imgs_batch)
                 img_features.append(imgs_features)
+        print(img_features.shape)
         img_features = torch.stack(img_features)
+        print(img_features.shape)
+
         img_features = img_features / img_features.norm(dim=-1, keepdim=True)
 
         # add index
